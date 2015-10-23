8d412d102ec7cc194c015ab23ee0a4169c3e3120
Al Ganiev <helios.ag@gmail.com>
2015-10-24 02:59:17 +1000

    DI configuration

---
+++ DependencyInjection/FMTinyMCEExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FM\TinyMCEBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * Class FMTinyMCEExtension
+ * @package FM\TinyMCEBundle\DependencyInjection
+ */
+class FMTinyMCEExtension extends Extension
+{
+
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('form.xml');
+        $loader->load('templating.xml');
+        $loader->load('twig.xml');
+
+        $container->setParameter('fm_tinymce', $config);
+        $container->setParameter('fm_tinymce.instances', $config['instances']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlias()
+    {
+        return 'fm_tinymce';
+    }
+
+}
