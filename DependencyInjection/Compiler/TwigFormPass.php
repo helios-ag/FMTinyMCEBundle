445f4f6435eefda1f8a7f1a074f809a67a1a03bd
Al Ganiev <helios.ag@gmail.com>
2015-10-24 02:58:54 +1000

    compiler pass

---
+++ DependencyInjection/Compiler/TwigFormPass.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FM\TinyMCEBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Class TwigFormPass
+ * @package FM\TinyMCEBundle\DependencyInjection\Compiler
+ */
+class TwigFormPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasParameter('twig.form.resources')) {
+            return;
+        }
+
+        $container->setParameter('twig.form.resources', array_merge(
+            array('FMTinyMCEBundle:Form:tinymce_widget.html.twig'),
+            $container->getParameter('twig.form.resources')
+        ));
+    }
+}
