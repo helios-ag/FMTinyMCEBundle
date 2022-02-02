c1e10038aded94c6328070e5ea3ff8870a166bef
Al Ganiev <helios.ag@gmail.com>
2022-02-02 23:39:32 +1000

    return type

--- src/FMTinyMCEBundle.php
+++ src/FMTinyMCEBundle.php
@@ -6,14 +6,8 @@ use FM\TinyMCEBundle\DependencyInjection\FMTinyMCEExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use FM\TinyMCEBundle\DependencyInjection\Compiler\TwigFormPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
-/**
- * Class TinyMCEBundle.
- *
- * @author Al Ganiev <helios.ag@gmail.com>
- * @copyright 2015- Al Ganiev
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
- */
 class FMTinyMCEBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
@@ -23,11 +17,7 @@ class FMTinyMCEBundle extends Bundle
         $container->addCompilerPass(new TwigFormPass());
     }
 
-    /**
-     * @return FMTinyMCEExtension
-     *                            Redefining root node
-     */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new FMTinyMCEExtension();
